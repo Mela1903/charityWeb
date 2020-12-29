004f26d79756fc3ef94a5407f9406e94626c9a44
Mela1903 <agnieszka.flak1@gmail.com>
2020-12-29 18:33:19 +0100

    Validation + save in DB

--- src/main/java/pl/coderslab/charity/controller/DonationController.java
+++ src/main/java/pl/coderslab/charity/controller/DonationController.java
@@ -3,7 +3,9 @@ package pl.coderslab.charity.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.Mapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 import pl.coderslab.charity.dto.DonationDto;
 import pl.coderslab.charity.entity.Category;
@@ -38,10 +40,11 @@ public class DonationController {
         return mav;
     }
 
+    @PostMapping("/donation/save")
     public ModelAndView saveDonationPost(@ModelAttribute("saveDonation") @Valid DonationDto donationDto){
 
         service.saveDonation(donationDto);
-        ModelAndView mav = new ModelAndView("redirect:donation/save");
+        ModelAndView mav = new ModelAndView("form-confirmation");
         mav.addObject("saveDonation", donationDto);
 
         return mav;
